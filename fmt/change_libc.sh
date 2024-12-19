31404bd4e2ff81059c49a6dc55636c414ec27434
alexdkfighter <13695242581@163.com>
2024-12-19 19:57:10 +0800

    增加文件夹分类

--- fmt/change_libc.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-patchelf --set-interpreter ./ld-linux-x86-64.so.2 ./pwn
-patchelf --replace-needed libc.so.6 ./libc.so.6 ./pwn
-patchelf --print-interpreter ./pwn
-patchelf --print-needed ./pwn
